093edb1333f6075ca7be19370d59752d2e20d21d
Eddy Caldas <eddycaldas@live.com>
2017-09-27 20:52:30 -0600

    reverseWord

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-var side1 = 5;
-var side2 = 6;
-var side3 = 7;
-var semiPerimeter = (side1 + side2 + side3)/2
 
-var area = Math.sqrt(semiPerimeter*(semiPerimeter-side1)*(semiPerimeter-side2)*(semiPerimeter-side3));
- document.write("Area of a triangle with the three sides 5, 6, 7 is :" + area)
+var myString = "Hello World";
+var emptyString = "";
+// splitString = myString.split("");
+// reverseString = splitString.reverse("");
+// joinString = reverseString.join ("")
+
+joinString = myString.split("").reverse("").join("")
+ document.write("Reverse 'Hello World': " + joinString)

9d21d6bfc103c0e04acdbef9602af54849d19a74
Eddy Caldas <eddycaldas@live.com>
2017-09-27 22:16:29 -0600

    animate-string

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-
-var myString = "Hello World";
-var emptyString = "";
-// splitString = myString.split("");
-// reverseString = splitString.reverse("");
-// joinString = reverseString.join ("")
-
-joinString = myString.split("").reverse("").join("")
- document.write("Reverse 'Hello World': " + joinString)
+function animate_string(id) {
+  var element = document.getElementById(id);
+  var textNode = element.childNodes[0]; //asuming no other childern
+  var text = textNode.data;
+  
+  setInterval(function() {
+    text = text[text.length-1] + text.substring(0, text.length-1);
+    textNode.data = text;
+  }, 320);
+}
